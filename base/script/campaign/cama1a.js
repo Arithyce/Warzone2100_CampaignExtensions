d79a7133968fa37887a241428706402cad47ed9d
Arithyce <108166240+Arithyce@users.noreply.github.com>
2022-06-25 00:37:13 -0400

    Add files via upload
    The entire mod thus far

---
+++ base/script/campaign/cama1a.js
@@ -0,0 +1,24 @@
+include("script/campaign/libcampaign.js");
+include("script/campaign/templates.js");
+
+
+function eventStartLevel()
+{
+	const PLAYER_POWER = 0;
+
+	camSetStandardWinLossConditions(CAM_VICTORY_STANDARD, "GAMMA_OUT");
+
+	if (difficulty === HARD)
+	{
+		setPower(0, CAM_HUMAN_PLAYER);
+	}
+	else if (difficulty === INSANE)
+	{
+		setPower(0, CAM_HUMAN_PLAYER);
+	}
+	else
+	{
+		setPower(PLAYER_POWER, CAM_HUMAN_PLAYER);
+	}
+
+}
