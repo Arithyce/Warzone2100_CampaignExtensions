60b344ec7e6226d22d8f2a250d3063f2fb78cd3b
Arithyce <108166240+Arithyce@users.noreply.github.com>
2022-07-02 15:06:50 -0400

    Update convert.cmd
    Remove the python script, you will have to manually convert labels.ini to labels.json.

--- mapfiles/tools/convert.cmd
+++ mapfiles/tools/convert.cmd
@@ -1,11 +1,8 @@
 mkdir "C:\Storage\Games & Apps\Warzone2100-MT\Output\map"
 cd "C:\Storage\Games & Apps\Warzone2100-MT"
 maptools map convert -t campaign -p 8 -f latest -i "C:\Storage\Games & Apps\Warzone2100-MT\Input\map" -o "C:\Storage\Games & Apps\Warzone2100-MT\Output\map"
-pause
+timeout /t 5 /nobreak
 move "C:\Storage\Games & Apps\Warzone2100-MT\Input\map.gam" "C:\Storage\Games & Apps\Warzone2100-MT\Output"
-pause
-python "C:\Storage\Games & Apps\Warzone2100-MT\ini2json.py" labels.ini
-pause
 cd "C:\Storage\Games & Apps\Warzone2100-MT\Input"
 rmdir /S /Q "C:\Storage\Games & Apps\Warzone2100-MT\Input"
 exit
