ee6b63ddb7152e086edf01fb001a37a2a9812adf
Arithyce <108166240+Arithyce@users.noreply.github.com>
2022-07-01 23:41:07 -0400

    Update to latest

--- base/script/campaign/cama1a.js
+++ base/script/campaign/cama1a.js
@@ -5,73 +5,30 @@ const SCAVENGER_RES = [
 	"R-Wpn-MG-Damage01", "R-Wpn-MG-ROF01",
 ];
 
-makeComponentAvailable("B1BaBaPerson01", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B4body-sml-trike01", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B3body-sml-buggy01", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B2JeepBody", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B2RKJeepBody", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B3bodyRKbuggy01", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BusBody", CAM_HUMAN_PLAYER)
-makeComponentAvailable("FireBody", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BaBaLegs", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BaBaProp", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaMG", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaTrikeMG", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaBuggyMG", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaJeepMG", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaRocket", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaBusCannon", CAM_HUMAN_PLAYER)
-makeComponentAvailable("BabaFlame", CAM_HUMAN_PLAYER)
-makeComponentAvailable("B2crane", CAM_HUMAN_PLAYER)
-makeComponentAvailable("scavCrane", CAM_HUMAN_PLAYER)
-
-makeComponentAvailable("B1BaBaPerson01", SCAV_7)
-makeComponentAvailable("B4body-sml-trike01", SCAV_7)
-makeComponentAvailable("B3body-sml-buggy01", SCAV_7)
-makeComponentAvailable("B2JeepBody", SCAV_7)
-makeComponentAvailable("B2RKJeepBody", SCAV_7)
-makeComponentAvailable("B3bodyRKbuggy01", SCAV_7)
-makeComponentAvailable("BusBody", SCAV_7)
-makeComponentAvailable("FireBody", SCAV_7)
-makeComponentAvailable("BaBaLegs", SCAV_7)
-makeComponentAvailable("BaBaProp", SCAV_7)
-makeComponentAvailable("BabaMG", SCAV_7)
-makeComponentAvailable("BabaTrikeMG", SCAV_7)
-makeComponentAvailable("BabaBuggyMG", SCAV_7)
-makeComponentAvailable("BabaJeepMG", SCAV_7)
-makeComponentAvailable("BabaRocket", SCAV_7)
-makeComponentAvailable("BabaBusCannon", SCAV_7)
-makeComponentAvailable("BabaFlame", SCAV_7)
-makeComponentAvailable("B2crane", SCAV_7)
-makeComponentAvailable("scavCrane", SCAV_7)
-
 function camEnemyBaseEliminated_scavGroup1()
 {
 	camEnableFactory("ScavFac");
-	camPlayVideos(["pcv456.ogg", {video: "MBA1A_MSG", type: MISS_MSG}]);
+	camPlayVideos({video: "MBA1A_MSG", type: MISS_MSG});
 }
 
 function camEnemyBaseEliminated_scavGroup2()
 {
-	camPlayVideos({video: "MBA1A1_MSG", type: MISS_MSG});
+	camPlayVideos({video: "MBA1A_MSG2", type: MISS_MSG});
 	const StructArti = [
 		"A0BaBaFlameTower", "A0CannonTower",
 	];
-		
-	const TempleArti = [
-		"BaBaPeople", "BabaBusCan",
-		"BabaFireCan", "BabaFireTruck",
-	];
-		
+	
 	for (let s = 0; s < StructArti.length; ++s)
 	{
 		enableStructure(StructArti[s], CAM_HUMAN_PLAYER);
 	};
-	
-	for (let t = 0; t < TempleArti.length; ++t)
-	{
-		enableTemplate(TempleArti[t]);
-	}
+	makeComponentAvailable("BabaFlame", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("BabaBusCannon", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("BabaMG", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("BaBaLegs", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("FireBody", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("BusBody", CAM_HUMAN_PLAYER);
+	makeComponentAvailable("B1BaBaPerson01", CAM_HUMAN_PLAYER);
 }
 
 function enableBaseStructures()
@@ -88,17 +45,17 @@ function enableBaseStructures()
 	}
 }
 
-function enableStartTemplates()
+function enableStartComponents()
 {
-	const TEMPLS = [
-		"BabaCrane", "BabaJeep",
-		"BarbarianBuggy", "BarbarianTrike",
-	];
-
-	for (let i = 0; i < TEMPLS.length; ++i)
-	{
-		enableTemplate(TEMPLS[i]);
-	}
+	makeComponentAvailable("B4body-sml-trike01", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("B3body-sml-buggy01", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("B2JeepBody", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("BaBaProp", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("BabaTrikeMG", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("BabaBuggyMG", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("BabaJeepMG", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("B2crane", CAM_HUMAN_PLAYER)
+	makeComponentAvailable("scavCrane", CAM_HUMAN_PLAYER)
 }
 
 changePlayerColour(CAM_HUMAN_PLAYER, 5)
@@ -126,7 +83,7 @@ function eventStartLevel()
 	}
 
 	enableBaseStructures();
-	enableStartTemplates();
+	enableStartComponents();
 	camCompleteRequiredResearch(SCAVENGER_RES, 7);
 
 	// Give player briefing.
